f23fbc79d7a7ae6c3a4ff76dd22153879f2d2513
Tracy <trac.boats.tb@gmail.com>
2025-11-10 21:34:50 -0600

    js changes

---
+++ assets/script/app.js
@@ -0,0 +1,41 @@
+const shapeSelect = document.getElementById('shapeSelect');
+const colorSelect = document.getElementById('colorSelect');
+const grid = document.getElementById('grid');
+const createBtn = document.getElementById('createBtn');
+const resetBtn = document.getElementById('resetBtn');
+
+const shapes = [];
+const MAX_SHAPES = 20;
+
+function createShape() {
+  
+  if (shapes.length >= MAX_SHAPES) {
+    alert("Game box is full! You can't add more shapes.");
+    return;
+  }
+
+  const shapeName = shapeSelect.value;
+  const color = colorSelect.value;
+
+  const newShape = new Shape(shapeName, color);
+  shapes.push(newShape);
+
+  const div = document.createElement('div');
+  div.classList.add('shape', shapeName);
+  div.style.backgroundColor = color;
+
+  div.addEventListener('click', () => {
+    alert(newShape.getInfo());
+  });
+
+  grid.appendChild(div);
+}
+
+function resetGame() {
+  grid.innerHTML = "";
+  shapes.length = 0;
+  alert("Game has been reset! You can start again.");
+}
+
+createBtn.addEventListener('click', createShape);
+resetBtn.addEventListener('click', resetGame);
---
+++ assets/script/utils.js
@@ -0,0 +1,18 @@
+class Shape {
+  constructor(name, colour) {
+    this._name = name;
+    this._colour = colour;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get colour() {
+    return this._colour;
+  }
+
+  getInfo() {
+    return `Shape: ${this.name}, Colour: ${this.colour}`;
+  }
+}
